ce5bbe696a63032e52a48dd54cdae18edcf98161
Himanshu First <himanshunaidu12161997@gmail.com>
2021-06-08 19:25:55 +0530

    Redundant File purged

--- WordMeaningsDB/sample_config.py
+++
@@ -1,4 +0,0 @@
-#RENAME TO config.py FOR USE
-
-words_table = 'word_meanings' #Table where the word meanings will be inserted
-reject_table = 'reject_words' #Table where the words that were rejected from being saved, will be inserted
